7114be27c1ad9626ed0b9cc0531793b665ce359a
gdrozo <gd.rozo@uniandes.edu.co>
2021-09-27 13:39:25 -0500

    -

--- test.py
+++ test.py
@@ -3,14 +3,15 @@ import time
 import threading
 
 wait_hours = 12  #Stop for 12 hours and then run again
-run_hours = 3/60    #We will run ngrep for an hour. The nth run will be dumped to net_log_n.txt
+run_hours = 1/60    #We will run ngrep for an hour. The nth run will be dumped to net_log_n.txt
 run_time_limit = 100    #Suppose you only want to take a log for 100 hours while you are away.
 
 def capture():
     ngrep_cmd = "sudo ngrep -W byline port 80"
     print('running process')
     try:
-        result = subprocess.check_output([ngrep_cmd], shell=True)
+        result = subprocess.check_output([ngrep_cmd], shell=True, stderr=subprocess.STDOUT, timeout=run_hours*3600)
+        print(result)
     except subprocess.CalledProcessError as e:
         print(e.output)
 
