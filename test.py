b00de836f9e4ba1b80781cd320e6ed0b9d8dbd76
gdrozo <gd.rozo@uniandes.edu.co>
2021-09-26 22:41:06 -0500

    -

--- test.py
+++ test.py
@@ -4,10 +4,10 @@ import re
 import time
 
 wait_hours = 12  #Stop for 12 hours and then run again
-run_hours = 0.05    #We will run ngrep for an hour. The nth run will be dumped to net_log_n.txt
+run_hours = 1    #We will run ngrep for an hour. The nth run will be dumped to net_log_n.txt
 run_time_limit = 100    #Suppose you only want to take a log for 100 hours while you are away.
 
-ngrep_cmd = "sudo ngrep -ixW >  net_log_" + str(fnum) + ".txt &"
+ngrep_cmd = "sudo ngrep -ixW >  net_log_.txt &"
 subprocess.call([ngrep_cmd], shell=True)
 time.sleep(run_hours*3600)
 subprocess.call(["sudo killall ngrep"], shell=True)
