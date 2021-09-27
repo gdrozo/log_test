b9af5b60e624b4494e720666b216f0f06f077478
gdrozo <gd.rozo@uniandes.edu.co>
2021-09-27 00:36:51 -0500

    -

--- test.py
+++ test.py
@@ -5,11 +5,11 @@ import time
 import threading
 
 wait_hours = 12  #Stop for 12 hours and then run again
-run_hours = 3/60    #We will run ngrep for an hour. The nth run will be dumped to net_log_n.txt
+run_hours = 1/60    #We will run ngrep for an hour. The nth run will be dumped to net_log_n.txt
 run_time_limit = 100    #Suppose you only want to take a log for 100 hours while you are away.
 
 def run():
-    ngrep_cmd = "sudo ngrep -ixW -W byline port 80> net_log_.txt"
+    ngrep_cmd = "sudo ngrep -W byline port 80> net_log_.txt"
     print('running process')
     subprocess.call([ngrep_cmd], shell=True)
 
