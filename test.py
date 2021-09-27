1c44d5ea852062ae69f0ccacae2a81b1ccd08fb3
gdrozo <gd.rozo@uniandes.edu.co>
2021-09-27 00:06:38 -0500

    -

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ wait_hours = 12  #Stop for 12 hours and then run again
 run_hours = 3/60    #We will run ngrep for an hour. The nth run will be dumped to net_log_n.txt
 run_time_limit = 100    #Suppose you only want to take a log for 100 hours while you are away.
 
-ngrep_cmd = "sudo ngrep -w byline port 80> net_log_.txt"
+ngrep_cmd = "sudo ngrep -W byline port 80> net_log_.txt"
 print('running process')
 subprocess.call([ngrep_cmd], shell=True)
 print('Sleeping')
